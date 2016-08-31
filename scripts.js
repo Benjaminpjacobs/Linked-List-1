0e58d054461e168f7f831712deff8f6a369e399d
Adam Rice <adamcoltonrice@gmail.com>
2016-08-31 07:33:19 -0600

    created first variables and setup some of P1 requirements

--- scripts.js
+++ scripts.js
@@ -1,5 +1,27 @@
-var bookmarkTitle = $('#cool')
+var title = $('#title')
+var url = $('#url')
+var create = $('#create')
+
 
 $(function() {
-  $('#cool').css("color","red")
+  // $('#cool').css("color","red")
+
+// When the user clicks on the button for creating the bookmark, it should be added to the bookmarks section
+
+
+//   When the user clicks on the “Mark as Read” button:
+// A class of .read should be added to the bookmark
+// If it already has the class of .read, it should be removed
+$('markAsRead').on('click', function() {
+  $(this).tr().toggleClass('read');
 });
+// When the user clicks on the “Remove” button, the link should be removed from the page
+$('remove').on('click', function() {
+  var row = $(this).closest('tr');
+  row.remove()
+});
+});
+
+create.addEventListener("click", function() {
+
+})
