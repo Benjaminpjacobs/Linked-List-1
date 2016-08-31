5092d05d225e6cf8dc62a4684dcbd3a015fe8195
Adam Rice <adamcoltonrice@gmail.com>
2016-08-31 11:11:40 -0600

    submit button functionality, converted table to list and added the remove and MARcheckbox

--- scripts.js
+++ scripts.js
@@ -1,27 +1,31 @@
-var title = $('#title')
-var url = $('#url')
+var $title = $('#title')
+var $url = $('#url')
 var create = $('#create')
 
 
 $(function() {
-  // $('#cool').css("color","red")
-
 // When the user clicks on the button for creating the bookmark, it should be added to the bookmarks section
+$('#create').on('click', function() {
+  $('ul').append(`
+    <li>
+    <span class='title'>${$title.val()}</span>
+    <span class='url'>${$url.val()}</span>
+    <button class='remove'>Remove</button>
+    <input type='checkbox' class='markAsRead'></input>
+    <li>
+  `);
+});
+});
 
 
 //   When the user clicks on the “Mark as Read” button:
 // A class of .read should be added to the bookmark
 // If it already has the class of .read, it should be removed
-$('markAsRead').on('click', function() {
-  $(this).tr().toggleClass('read');
-});
-// When the user clicks on the “Remove” button, the link should be removed from the page
-$('remove').on('click', function() {
-  var row = $(this).closest('tr');
-  row.remove()
-});
-});
-
-create.addEventListener("click", function() {
-
-})
+// $('markAsRead').on('click', function() {
+//   $(this).tr().toggleClass('read');
+// });
+// // When the user clicks on the “Remove” button, the link should be removed from the page
+// $('remove').on('click', function() {
+//   var row = $(this).closest('tr');
+//   row.remove()
+// });
