f5a088c2f5109d02d8490d5e2e9b9d322f2d3a6a
Christine Gamble <christine.c.gamble@gmail.com>
2016-08-31 08:05:45 -0600

    working on error message, disabling submit button, and creating the counter

--- scripts.js
+++ scripts.js
@@ -3,3 +3,30 @@ var bookmarkTitle = $('#cool')
 $(function() {
   $('#cool').css("color","red")
 });
+
+
+function enableBtn() {
+  if ($('#url').val() === "" && $('#title').val() === "") {
+    return $('#create').attr('disabled', true); }
+  if ($('#url').val() !== "" && $('#bookmark-title').val() !== "") {
+    return $('#create').attr('disabled', false);}
+}
+
+  var clearField = function() {
+    $('#title').val('');
+    $('#url').val('');
+  }
+  // $('#create').click( function() {
+  //   if ($('#url').val() === '' || $('#title').val() === '') {
+  //     alert("ERROR: Please enter Bookmark Information"); }
+  //   });
+
+  clearField()
+  enableBtn()
+
+  //to create counter,
+  //$('.counter').html('<p>' BookmarkCounter + ' total bookmark(s). </p>');
+  //$('.counter').append('<p>'unreadCounter + 'unread bookmark(s). </p>');
+  //bookmarkCounter() { $('.bookmarkTitle').length});
+  //readcounter() { $('.read').length}
+  //unreadCounter() { ($('.bookmarkTitle').length - $('.read').length)};
