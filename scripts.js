72b57f4f2eb80d7b9b8c630a0a092869a6b6d150
Christine Gamble <christine.c.gamble@gmail.com>
2016-08-31 21:26:16 -0600

    refactor JS

--- scripts.js
+++ scripts.js
@@ -1,58 +1,56 @@
 var $title = $('#title')
 var $url = $('#url')
-var $read = $('.read')
 var $create = $('#create')
-var $data = $('li')
+var clearField = function() {
+  $title.val('');
+  $url.val('');
+}
 
 
-$('.list').on('click', function() {
-  countAll();
-});
-
-
-$('#create').on('click', function() {
+$create.on('click', function() {
   validateInputs()
-  $('ul').append(`
-    <li class="unread">
-    <span class='title'>${$title.val()}</span>
-    <span class='url'>${$url.val()}</span>
-    <button class='remove'>Remove</button>
-    <input type='checkbox' class='markAsRead'></input>
-    </li>
-  `);
+  addLink()
   clearField()
-  $('#create').attr('disabled', 'disabled')
+  $create.attr('disabled', 'disabled')
   countAll();
 });
-
 
 $('.list').on('click', '.markAsRead', function() {
   $(this).parent().toggleClass('read');
   $(this).parent().toggleClass('unread');
+  countAll();
 });
 
 $('.list').on('click', '.remove', function() {
   $(this).parent().remove();
+  countAll();
 });
 
+enableBtn()
 
-function enableBtn() {
-  if ($('#url').val() === "" || $('#title').val() === "") {
-    return $('#create').attr('disabled', true); }
-  if ($('#url').val() !== "" || $('#bookmark-title').val() !== "") {
-    return $('#create').attr('disabled', false);}
+function addLink() {
+    $('ul').append(`
+   <li class="unread">
+   <span class='title'>${$title.val()}</span>
+   <span class='url'>${$url.val()}</span>
+   <button class='remove'>Remove</button>
+   <input type='checkbox' class='markAsRead'></input>
+   </li>
+ `);
 }
 
-  var clearField = function() {
-    $('#title').val('');
-    $('#url').val('');
-  }
-  //comment out enableBtn on create button to see error
-  function validateInputs(){
-    if ($('#url').val() === '' || $('#title').val() === '') {
-      alert("ERROR: Please enter Bookmark Information"); }
-  }
+function enableBtn() {
+  if ($('#url').val() === "" || $title.val() === "") {
+    return $create.attr('disabled', true); }
+  if ($('#url').val() !== "" && $('#bookmark-title').val() !== "") {
+    return $create.attr('disabled', false);}
+}
 
+  function validateInputs(){
+    if ($url.val() === '' || $title.val() === '') {
+      alert("ERROR: Please enter Bookmark Information");
+    }
+  }
 
 function countLinks(){
   return $('li').length
@@ -71,5 +69,3 @@ function countAll () {
   $('.read-links').text('Number of Read Links: ' + totalReadLinks());
   $('.unread-links').text('Number of Unread Links: ' + unreadLinks());
 }
-
-enableBtn()
